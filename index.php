443d17549b5663b076ce09b81dfba94daed2e428
Sajidur Rahman <shajidur171@gmail.com>
2023-02-01 17:42:40 +0600

    V2

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+$db = mysqli_connect("localhost", "root", "", "data_insert");
+if (!$db) {
+    echo mysqli_connect_error();
+}
+
+if (isset($_POST['submit'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+
+    // Getting the image
+    $imagename = $FILES['image']['name'];
+    $tmplocation = $FILES['image']['tmp_name'];
+
+    $uplocation = 'images/' . $imagename;
+
+    
+}
 
 ?>
 <!DOCTYPE html>
@@ -16,7 +34,7 @@
 <body>
     <div class="form-container">
         <h1 class="form-title">Send Your Record</h1>
-        <form action="index.php" method="POST" enctype="multipart/form-data">
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data">
             <input type="text" name="firstname" class="form-input" placeholder="First Name">
             <input type="text" name="lastname" class="form-input" placeholder="Last Name">
             <input type="email" class="form-input" name="email" placeholder="Email"> <br>
